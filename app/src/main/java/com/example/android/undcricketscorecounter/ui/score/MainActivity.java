79f59052495e7e69cbe8852624b69c997473066b
Nishant Patel <nishantuwindsor@gmail.com>
2018-05-28 22:34:28 -0400

    Add menu item to reset game

--- app/src/main/java/com/example/android/undcricketscorecounter/ui/score/MainActivity.java
+++ app/src/main/java/com/example/android/undcricketscorecounter/ui/score/MainActivity.java
@@ -33,6 +33,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 
 import com.example.android.undcricketscorecounter.R;
@@ -124,5 +126,22 @@ public class MainActivity extends AppCompatActivity
                                     }
                                 });
         builder.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_reset:
+                presenter.resetGame();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
